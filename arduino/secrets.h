063641a86926ffbff25ac507de75b699d343a817
nagurao <nagurao.basude@gmail.com>
2022-12-06 01:00:01 +0530

    Auto commit on 06-12-2022 01:00:01

---
+++ arduino/secrets.h
@@ -0,0 +1,6 @@
+const char* PROGMEM host = "NetMeter";
+const char* PROGMEM update_path = "/firmware";
+const char* PROGMEM update_username = "ESPNode";
+const char* PROGMEM update_password = "ESPN0de";
+const char* PROGMEM AP_ssid = "ESPNode";
+const char* PROGMEM AP_password = "ESPN0de";
--- autoupdate.sh
+++ autoupdate.sh
@@ -2,4 +2,3 @@ cd /var/www/mysite
 git add .
 git commit -m "Auto commit on `date +'%d-%m-%Y %H:%M:%S'`"
 git push -u origin master
-
