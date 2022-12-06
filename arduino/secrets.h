654b73a3ab79467a0bfbcc9f27c59b7d5960cdfc
nagurao <nagurao@users.noreply.github.com>
2022-12-06 10:44:19 +0530

    Delete secrets.h

--- arduino/secrets.h
+++
@@ -1,6 +0,0 @@
-const char* PROGMEM host = "NetMeter";
-const char* PROGMEM update_path = "/firmware";
-const char* PROGMEM update_username = "ESPNode";
-const char* PROGMEM update_password = "ESPN0de";
-const char* PROGMEM AP_ssid = "ESPNode";
-const char* PROGMEM AP_password = "ESPN0de";
