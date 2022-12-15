3e5e6f0ed40c525b4119481085e13af9c70b2bc7
nagurao <nagurao.basude@gmail.com>
2022-12-16 00:36:01 +0530

    Auto commit on 16-12-2022 00:36:01

--- autobackup.sh
+++ autobackup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 rm /var/www/mysite/*.gz
 mysqldump -unagu -pnagu -P3306 -hlocalhost readings | gzip -c > /var/www/mysite/db_backup.gz
+cd /var/www/mysite/
 cp *.gz /home/lamp/mysite/`date +'%d-%m-%Y_%H:%M:%S'`.gz
